01b42c8eaab6799889b19a56de477cb3bc47d63b
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2023-09-28 01:58:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  }
+  return messages;
+}
+function countDown(Number) {
+  let countDownNum = Number;
+  while (countDownNum >= 0) {
+    console.log(countDownNum);
+    countDownNum--;
+  }
+}
